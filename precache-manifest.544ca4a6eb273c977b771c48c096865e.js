cb8e3732c752d6f6404301b6b09ec6c1f15b8fd4
Erion Muca <eriuni@gmail.com>
2019-04-30 23:50:19 +0200

    Updates

---
+++ precache-manifest.544ca4a6eb273c977b771c48c096865e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98a4b61091de6d02e29d6fb1d2512d1",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "a86d7341ada25fa8c568",
+    "url": "/calculator/static/css/main.f70d7270.chunk.css"
+  },
+  {
+    "revision": "b000fccb79e89af31dc4",
+    "url": "/calculator/static/js/2.abf43ca4.chunk.js"
+  },
+  {
+    "revision": "a86d7341ada25fa8c568",
+    "url": "/calculator/static/js/main.ab3de30c.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
